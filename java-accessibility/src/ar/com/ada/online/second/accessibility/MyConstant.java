793522a7f4d139234ef240317c8345242be15c1d
Fiore Carusso <fiore.carusso@hotmail.com.ar>
2020-09-17 11:55:28 -0300

    Agrego clase MyConstant

---
+++ java-accessibility/src/ar/com/ada/online/second/accessibility/MyConstant.java
@@ -0,0 +1,13 @@
+package ar.com.ada.online.second.accessibility;
+
+public class MyConstant {
+
+
+    public static final double pi = 3.14159;
+    static Integer counter;
+
+    public static void showPiValue() {
+        System.out.println("El valor de Pi es: " + pi );
+    }
+
+}
