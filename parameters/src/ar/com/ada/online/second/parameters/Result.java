7ce50198f4166a0733c3f6a6b9857e6a14c11cdc
Fiore Carusso <fiore.carusso@hotmail.com.ar>
2020-09-17 22:11:56 -0300

    Ejercicio de paso de variables

---
+++ parameters/src/ar/com/ada/online/second/parameters/Result.java
@@ -0,0 +1,17 @@
+package ar.com.ada.online.second.parameters;
+
+public class Result {
+
+    private Integer resultSum;
+    private Integer resultSubtraction;
+    private Integer multiplicacion;
+    private Integer division;
+
+    public Integer getResultSum() {
+        return resultSum;
+    }
+
+    public void setResultSum(Integer resultSum) {
+        this.resultSum = resultSum;
+    }
+}
